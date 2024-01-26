d6a62e6eb84e59a10fe862a22b7842ba75182ac5
hoqradwan <92720128+hoqradwan@users.noreply.github.com>
2024-01-26 07:56:02 +0600

    project setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@material-tailwind/react"
+
+
+function App() {
+
+  return (
+    <Button className="text-green-500" >
+      My Boy
+    </Button>
+  )
+}
+
+export default App
